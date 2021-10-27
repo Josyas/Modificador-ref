442d5e755560963992a413bba296ed6f9b7870d5
Josias <87546094+Josyas@users.noreply.github.com>
2021-10-27 06:15:42 -0300

    Add files via upload

---
+++ Modificadores_de_parâmetros_ref/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace Modificadores_de_parâmetros_ref_e_out {
+    class Caculator {
+
+        public static void Triple(ref int x) {
+            x = x * 3;
+        }
+
+    }
+}
---
+++ Modificadores_de_parâmetros_ref/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modificadores_de_parâmetros_ref_e_out {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = 10;
+            Caculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
